ee30382d33d5f52e136d264efd04fe3e0b003975
noudas <noudas1@gmail.com>
2025-02-25 13:35:40 +0200

    adding tailwind

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.tsx'
 import store from './redux/store/store.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
