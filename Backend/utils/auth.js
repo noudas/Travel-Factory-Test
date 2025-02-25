e18305a0cbb9c8c68638231654be4f333fba9d1a
noudas <noudas1@gmail.com>
2025-02-25 11:32:49 +0200

    JWT Generator

---
+++ Backend/utils/auth.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+function generateToken(user) {
+    return jwt.sign(
+        { id: user.id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: '2h' }
+    );
+}
