33ec45b800b4eabb1e28f651fbd8a90264606cf4
noudas <noudas1@gmail.com>
2025-03-04 13:39:54 +0200

    Token API working of fetching

--- Frontend/src/api/api.tsx
+++ Frontend/src/api/api.tsx
@@ -9,7 +9,6 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-    console.log(token);
   }
   return config;
 });
