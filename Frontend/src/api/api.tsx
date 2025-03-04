1d58c60c7acdee3962c39d8890cb2708819b4dd3
noudas <noudas1@gmail.com>
2025-03-04 13:39:04 +0200

    Test V2 start

--- Frontend/src/api/api.tsx
+++ Frontend/src/api/api.tsx
@@ -9,6 +9,7 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
+    console.log(token);
   }
   return config;
 });
